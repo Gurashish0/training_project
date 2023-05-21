257198da5c84f9d2dc30907f8364068d7d00e5af
Gurashish Singh <virkguri3333@gmail.com>
2023-05-21 23:13:40 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        // Some useful comment
+        fontFamily: {
+            'nunito': ['nunito', 'sans-serif'],
+            'MyFont': ['"Pacifico"', 'Pacifico']
+        },
+    },
+    variants: {
+        // Some useful comment
+    },
+    plugins: [
+        // Some useful comment
+    ]
+}
